4c3b2c3295249a79252d3c64ebc6b41be602f61c
František Gič <mcfreddie777@gmail.com>
2019-11-30 14:29:16 +0100

    Created game base file

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,52 @@
 // Created by František Gič on 18/11/2019.
 //
 
+#include <iostream>
+#include <list>
+#include <bin/ppgso/lib/ppgso.h>
+#include <GLUT/glut.h>
+
+using namespace std;
+using namespace glm;
+using namespace ppgso;
+
+const unsigned int SIZE = 1024;
+
+class SceneWindow : public Window {
+
+private:
+    void initScene() {
+    }
+
+public:
+    SceneWindow() : Window{"DYNABlaster3D", SIZE, SIZE} {
+        hideCursor();
+        glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
+        
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LEQUAL);
+        
+        glEnable(GL_CULL_FACE);
+        glFrontFace(GL_CCW);
+        glCullFace(GL_BACK);
+        
+        initScene();
+    }
+    
+    void onKey(int key, int scanCode, int action, int mods) override {
+    }
+    
+    
+    void onIdle() override {
+        glClearColor(.5f, .5f, .5f, 0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+};
+
 int main() {
-    return 0;
+    SceneWindow window;
+    
+    while (window.pollEvents()) {}
+    
+    return EXIT_SUCCESS;
 }
